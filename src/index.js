64de46c16a4a2aba257df872af94af5963a6f2ec
Spotted Metal <38660687+spotted-metal@users.noreply.github.com>
2022-10-20 11:32:31 +0800

    Add react-dom dependency

--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@
  */
 
 import {AppRegistry} from 'react-native';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>
+)
